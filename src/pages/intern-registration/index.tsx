e1ff93750479c457fa4b12bf1ef6f7926933d924
Syazantri Salsabila <syazantri@gmail.com>
2024-10-01 19:49:36 +0700

    fix

--- src/pages/intern-registration/index.tsx
+++ src/pages/intern-registration/index.tsx
@@ -155,7 +155,7 @@ const Register: React.FC = () => {
             {loading ? (
               <div className="flex justify-center items-center">
                 <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-                  <span className="visually-hidden">Loading...</span>
+                  <span className="visually-hidden"></span>
                 </div>
               </div>
             ) : (
--- src/pages/lapor/index.tsx
+++ src/pages/lapor/index.tsx
@@ -139,7 +139,7 @@ const Report: React.FC = () => {
             {loading ? (
               <div className="flex justify-center items-center">
                 <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-                  <span className="visually-hidden">Loading...</span>
+                  <span className="visually-hidden"></span>
                 </div>
               </div>
             ) : (
